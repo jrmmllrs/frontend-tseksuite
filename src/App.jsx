5278c2940a08bfe510ca078e43eb584a457143dd
francisdarang-maker <francis.darang@getfullsuite.com>
2025-10-30 07:38:09 +0800

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex justify-center items-center'>
+        Okay na to
+      </div>
+    </>
+  )
+}
+
+export default App
