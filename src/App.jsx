370661c058864206c14eb33de79e0cb53f568aa2
Neil Pascual <neilpascual@Mac.localdomain>
2025-11-03 13:27:39 +0800

    feat: dashboard page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router'
 import OnboardingPage from './pages/applicant/OnboardingPage' 
+import DashboardPage from './pages/admin/DashboardPage'
 function App() {
 
   return (
@@ -7,6 +8,7 @@ function App() {
       <Routes>
         <Route path='/' element={ <OnboardingPage/> }/>
         {/* /admin */}
+        <Route path='/admin/dashboard' element={ <DashboardPage/>} />
       </Routes>
    </BrowserRouter>
   )
