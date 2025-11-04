03c19e6e1746d494c27965c3eef58035e8cdd13e
Anonyyymouse <ravalbert17@gmail.com>
2025-11-05 07:57:58 +0800

    Feat: Refactored AdminSideBar

--- src/layouts/admin/MainLayout.jsx
+++ src/layouts/admin/MainLayout.jsx
@@ -1,18 +1,17 @@
 import { Outlet } from "react-router";
-import AdminSideBar from "../../components/admin/AdminSideBar";
+import AdminSideBar from "../../components/admin/AdminSidebar/AdminSideBar";
 function MainLayout() {
   return (
-    <div className='flex overflow-hidden'>
+    <div className="flex overflow-hidden">
+      {/* Components Sidebar */}
+      <AdminSideBar />
 
-        {/* Components Sidebar */}
-        <AdminSideBar/>
-
-        {/* Content here */}
-        <section className='h-screen w-full p-2 sm:px-5 sm:py-1 md:px-10 md:py-2.5 lg:px-15 lg:py-5 xl:px-35 xl:py-10 bg-[#F9FAFB] overflow-y-auto'>
-            <Outlet/>
-        </section>
+      {/* Content here */}
+      <section className="h-screen w-full p-2 sm:px-5 sm:py-1 md:px-10 md:py-2.5 lg:px-15 lg:py-5 xl:px-35 xl:py-10 bg-[#F9FAFB] overflow-y-auto">
+        <Outlet />
+      </section>
     </div>
-  )
+  );
 }
 
 export default MainLayout;
